e0007e1518e1bc24131b8f548334ceaab1ff3fd2
Yean Lee <talk2yeanlee@gmail.com>
2025-04-25 16:09:57 +0900

    made four buttons at dept of j page

--- app/dept-of-j/index.tsx
+++ app/dept-of-j/index.tsx
@@ -1,7 +1,28 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { commonStyles } from '../../constants/ThemeStyles';
 
 
 export default function Index() {
+  const handlePress = (buttonName: string) => {
+    console.log(`${buttonName} 버튼이 눌렸습니다.`);
+  };
+
   return (
-    <></>
-  )
+    <View style={commonStyles.container}>
+      <TouchableOpacity style={commonStyles.button} onPress={() => handlePress('버튼 1')}>
+        <Text style={commonStyles.buttonText}>법정 리스트 다운받기</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={commonStyles.button} onPress={() => handlePress('버튼 2')}>
+        <Text style={commonStyles.buttonText}>법정 리스트 업로드</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={commonStyles.button} onPress={() => handlePress('버튼 3')}>
+        <Text style={commonStyles.buttonText}>누계 파일 다운받기</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={commonStyles.button} onPress={() => handlePress('버튼 4')}>
+        <Text style={commonStyles.buttonText}>누계 파일 업로드</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
+
