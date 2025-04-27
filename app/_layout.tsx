9754f187673acb63db36145505ae913e85cbecba
Yoo-Chan Jo <jo.yoochani@gmail.com>
2025-04-27 21:28:31 +0900

    Added firebase package

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,6 +10,8 @@ import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
+// import 
+
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
@@ -32,10 +34,9 @@ export default function RootLayout() {
   return (
     <GluestackUIProvider mode="light"><ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <Stack>
-          {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
           <Stack.Screen name="+not-found" />
         </Stack>
         <StatusBar style="auto" />
-      </ThemeProvider></GluestackUIProvider>
+    </ThemeProvider></GluestackUIProvider>
   );
 }
