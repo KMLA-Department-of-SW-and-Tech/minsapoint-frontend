f7d2868540b15a26d410385feca64b019b71b7ed
Yoo-Chan Jo <jo.yoochani@gmail.com>
2025-05-27 11:19:53 +0900

    Removed sensitive stuff

--- api/components/UserLoginButton.jsx
+++ api/components/UserLoginButton.jsx
@@ -5,10 +5,10 @@ import { GoogleAuthProvider, signInWithCredential } from "firebase/auth";
 import { auth } from "@/api/config/firebase";
 import { useEffect } from 'react';
 import { Button } from '@gluestack-ui/themed';
-import { Platform } from 'react-native';
+// import { Platform } from 'react-native';
 
 // only for dev, the client_secret should be placed in environment variables
-import { expo, web } from "@/api/dev/google-client/google_client_secret.json";
+import { googleLoginConfig } from "@/config/google_login";
 
 const syncFirebaseWithMongoose = async (credential) => {
   try {
@@ -27,82 +27,86 @@ const syncFirebaseWithMongoose = async (credential) => {
 export default function UserLoginButton({ style, children }) {
   // logic problem needs bugfix
 
-  const [request, response, promptAsync] = Google.useAuthRequest({
-    expoClientId: expo.client_id,
-    // iosClientId: 'YOUR_IOS_CLIENT_ID.apps.googleusercontent.com',
-    // androidClientId: 'YOUR_ANDROID_CLIENT_ID.apps.googleusercontent.com',
-    webClientId: web.client_id,
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+    clientId: googleLoginConfig.expo.client_id
   });
 
-  // PROBLEM
-  // Function to handle sign-in and token exchange
-  const signInWithPopupInWebAndSyncDB = () => {
-    const url = `https://accounts.google.com/o/oauth2/v2/auth?response_type=token&client_id=${web.client_id}&redirect_uri=${encodeURIComponent("http://localhost:8081/")}`;
+  // // PROBLEM
+  // // Function to handle sign-in and token exchange
+  // const signInWithPopupInWebAndSyncDB = () => {
+  //   const url = `https://accounts.google.com/o/oauth2/v2/auth?response_type=token&client_id=${web.client_id}&redirect_uri=${encodeURIComponent("http://localhost:8081/")}`;
     
-    const popupWindow = window.open(url, 'GoogleAuth', 'width=600,height=600');
+  //   const popupWindow = window.open(url, 'GoogleAuth', 'width=600,height=600');
 
-    // Handle window closing and successful login
-    popupWindow.addEventListener('load', async () => {
-      const urlParams = new URLSearchParams(popupWindow.location.hash.substring(1));
-      const idToken = urlParams.get('id_token');
-      // const provider = new GoogleAuthProvider();
-      // provider.setCustomParameters({
-      //   prompt: "select_account", // This forces the account selection account every time
-      // });
-      if (idToken) {
-        const firebaseResult = await signInWithCredential(auth, GoogleAuthProvider().credential(idToken));
-        console.log('Firebase sign-in result: ', firebaseResult);
-        // Continue   your app flow
+  //   // Handle window closing and successful login
+  //   popupWindow.addEventListener('load', async () => {
+  //     const urlParams = new URLSearchParams(popupWindow.location.hash.substring(1));
+  //     const idToken = urlParams.get('id_token');
+  //     // const provider = new GoogleAuthProvider();
+  //     // provider.setCustomParameters({
+  //     //   prompt: "select_account", // This forces the account selection account every time
+  //     // });
+  //     if (idToken) {
+  //       const firebaseResult = await signInWithCredential(auth, GoogleAuthProvider().credential(idToken));
+  //       console.log('Firebase sign-in result: ', firebaseResult);
+  //       // Continue   your app flow
 
-        // check whether user exists and create account if not
-        // if (result) await syncFirebaseWithMongoose(result);
-        // check admin
-        // const userInfo = await axiosPrivate.get(
-        //   "/api/user",
-        //   result.user.accessToken
-        // );
-      } else {
-        console.error("Failed to retrieve ID token from the popup.");
-      }
-      popupWindow.close();
-    });
-  };
-  const signInWithPopupInNonWebAndSyncDB = async () => {
-    const provider = new GoogleAuthProvider();
-    provider.setCustomParameters({
-      prompt: "select_account", // This forces the account selection account every time
-    });
+  //       // check whether user exists and create account if not
+  //       // if (result) await syncFirebaseWithMongoose(result);
+  //       // check admin
+  //       // const userInfo = await axiosPrivate.get(
+  //       //   "/api/user",
+  //       //   result.user.accessToken
+  //       // );
+  //     } else {
+  //       console.error("Failed to retrieve ID token from the popup.");
+  //     }
+  //     popupWindow.close();
+  //   });
+  // };
+  // const signInWithPopupInNonWebAndSyncDB = async () => {
+  //   const provider = new GoogleAuthProvider();
+  //   provider.setCustomParameters({
+  //     prompt: "select_account", // This forces the account selection account every time
+  //   });
 
-    // Step 1: Get the Google user's ID token
-    const { id_token } = response.params;
+  //   // Step 1: Get the Google user's ID token
+  //   const { id_token } = response.params;
 
-    // Step 2: Create a Firebase credential with the ID token
-    const googleCredential = provider.credential(id_token);
+  //   // Step 2: Create a Firebase credential with the ID token
+  //   const googleCredential = provider.credential(id_token);
 
-    // Step 3: Sign in with Firebase using the credential
-    const firebaseResult = await signInWithCredential(auth, googleCredential);
-    console.log("Firebase result: ", firebaseResult);
+  //   // Step 3: Sign in with Firebase using the credential
+  //   const firebaseResult = await signInWithCredential(auth, googleCredential);
+  //   console.log("Firebase result: ", firebaseResult);
 
-    // check whether user exists and create account if not
-    // if (result) await syncFirebaseWithMongoose(result);
-    // check admin
-    // const userInfo = await axiosPrivate.get(
-    //   "/api/user",
-    //   result.user.accessToken
-    // );
+  //   // check whether user exists and create account if not
+  //   // if (result) await syncFirebaseWithMongoose(result);
+  //   // check admin
+  //   // const userInfo = await axiosPrivate.get(
+  //   //   "/api/user",
+  //   //   result.user.accessToken
+  //   // );
 
-  }
-  const signInAndSyncDB = () => {
-    if (Platform.OS === 'web') {
-      signInWithPopupInWebAndSyncDB();
-    } else {
-      signInWithPopupInNonWebAndSyncDB();
-    }
-  }
+  // }
+  // const signInAndSyncDB = () => {
+  //   if (Platform.OS === 'web') {
+  //     signInWithPopupInWebAndSyncDB();
+  //   } else {
+  //     signInWithPopupInNonWebAndSyncDB();
+  //   }
+  // }
 
   useEffect(() => {
     if (response?.type === 'success'){
-      signInAndSyncDB();
+      // signInAndSyncDB();
+      const { id_token } = response.params;
+      const credential = GoogleAuthProvider.credential(id_token);
+      signInWithCredential(auth, credential).then((userCredential) => {
+          setUser(userCredential.user);
+        }).catch((error) => {
+          console.error("Firebase auth error:", error);
+      });
     }
   }, [response]);
 
@@ -123,16 +127,22 @@ export default function UserLoginButton({ style, children }) {
 const signUserOut = async (accessToken) => {
   try {
     if (accessToken) {
-      await auth.signOut();
-      console.log('Revoking token:', accessToken)
+      console.log("Revoking token:", accessToken);
+
+      // Step 1: Revoke Google token
       await AuthSession.revokeAsync(
         { token: accessToken },
-        { revocationEndpoint: 'https://oauth2.googleapis.com/revoke' }
+        { revocationEndpoint: "https://oauth2.googleapis.com/revoke" }
       );
+
+      // Step 2: Sign out from Firebase
+      await signOut(auth);
+
+      console.log("User signed out and token revoked.");
     } else {
       throw new Error("No access token provided");
     }
-  } catch(e) {
+  } catch (e) {
     console.error("Failed to log out: ", e);
   }
 };
